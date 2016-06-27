36ea05a6ff074154d2ce3dd33bd4d79f4a0ba9ee
yye <280798744@qq.com>
2016-06-27 16:11:35 +0800

    优化

--- MVVM--Demo/Utils/NetAPIClient.m
+++ MVVM--Demo/Utils/NetAPIClient.m
@@ -5,10 +5,18 @@
 //  Created by Apple on 16/6/21.
 //  Copyright © 2016年 杨永恩. All rights reserved.
 //
-
+/**
+ 
+ *******************************************************
+ *                                                      *
+ * 感谢您的支持， 如果下载的代码在使用过程中出现BUG或者其他问题    *
+ * 您可以发邮件到muyang_xl@sina.com 或者 到                    *
+ * https://github.com/muyang00?tab=repositories 提交问题   *
+ *                                                      *
+ *******************************************************
+ 
+ */
 #import "NetAPIClient.h"
-
-
 
 #define BASEURL @"http://platform.sina.com.cn/sports_all/client_api?"
 
